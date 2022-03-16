d44c29826220be913ed5a7bf88d9d16576b0d248
Nidhi Sharma <31341830+Niddhisharma3@users.noreply.github.com>
2022-03-16 22:19:49 +0530

    Add files via upload

---
+++ NotOperatorExample.java
@@ -0,0 +1,31 @@
+package Day3;
+//not operator  ! 
+
+public class NotOperatorExample {
+
+	public static void main(String[] args) {
+
+
+		boolean r= true;
+		System.out.println(r); //true
+		System.out.println(!r);//false
+		System.out.println( !(10==20));//true
+		System.out.println(10!=20); //true
+		int code=123;
+		if( code!=123)
+			System.out.println(" code doesnot match");
+		int a=10;
+	 	if( !(a>20))
+			System.out.println("welcome");
+		else
+			System.out.println("sorry");
+	}
+
+}
+
+
+
+
+
+
+
